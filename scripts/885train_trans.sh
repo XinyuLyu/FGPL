4e1300a9d41d11a46fec03ac155d112b0a7ea87c
Xinyu Lyu <xinyulyu68@gmail.com>
2022-04-06 16:09:41 +0800

    Update 885train_trans.sh

--- scripts/885train_trans.sh
+++ scripts/885train_trans.sh
@@ -7,6 +7,7 @@ if [ $1 == "0" ]; then
     mode="Predcls_"
     MODEL="transformer_FGPL"
     MODEL_NAME=${mode}${MODEL}
+    mkdir ./checkpoints/${MODEL_NAME}/
     python ./tools/relation_train_net.py \
     --config-file "configs/e2e_relation_X_101_32_8_FPN_1x_transformer_FGPL.yaml" \
     MODEL.ROI_RELATION_HEAD.USE_GT_BOX True \
